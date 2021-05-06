cea5a5d81397edf96dbdb3a5466f68540c1344f6
Yaozong Liu <750188453@qq.com>
2021-05-06 22:34:56 +0800

    Revert "fix: interactiveGroupBy can works regardless to queryMode"
    This reverts commit bdbcfc3fe2b0731946f144bd14f36ddca6132e6b.

--- plugins/plugin-chart-table/src/buildQuery.ts
+++ plugins/plugin-chart-table/src/buildQuery.ts
@@ -127,7 +127,7 @@ const buildQuery: BuildQuery<TableChartFormData> = (formData: TableChartFormData
     }
 
     const interactiveGroupBy = formData.extra_form_data?.interactive_groupby;
-    if (interactiveGroupBy && queryObject.columns) {
+    if (interactiveGroupBy && queryObject.columns && queryMode === QueryMode.aggregate) {
       queryObject.columns = [...new Set([...queryObject.columns, ...interactiveGroupBy])];
     }
 
