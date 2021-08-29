5243a8e84bc23f3daf3c91f1d9120b25c5e334b1
stephenLYZ <750188453@qq.com>
2021-08-29 21:04:05 +0800

    fix: type

--- plugins/plugin-chart-echarts/src/MixedTimeseries/transformProps.ts
+++ plugins/plugin-chart-echarts/src/MixedTimeseries/transformProps.ts
@@ -145,7 +145,7 @@ export default function transformProps(
       markerSize,
       areaOpacity: opacity,
       seriesType,
-      stack,
+      stack: Boolean(stack),
       yAxisIndex,
       filterState,
     });
@@ -158,7 +158,7 @@ export default function transformProps(
       markerSize: markerSizeB,
       areaOpacity: opacityB,
       seriesType: seriesTypeB,
-      stack: stackB,
+      stack: Boolean(stackB),
       yAxisIndex: yAxisIndexB,
       filterState,
     });
